882862342f03c2b7591d780e7928bc0602e130bf
kmcgraw <kmcgraw@caltech.edu>
2020-07-19 15:10:09 -0700

    Fixed variable names for predictor and outcomes latex tables, and added rows with percentages for the predictors

--- analytics/demographics_by_year.R
+++ analytics/demographics_by_year.R
@@ -25,16 +25,22 @@ new_pred18 <- mutate(pred_puf18,
                      age_51_to_65 = if_else(patient_age >= 51 & patient_age <= 65, 1, 0, missing=0),
                      age_66_plus = if_else(patient_age >= 66, 1, 0, missing=0),
 )
-demo_names <- c("male", "female", "age_18_to_24", "age_25_to_35", "age_36_to_50", "age_51_to_65", "age_66_plus", "race_asian", "race_black", "race_nativeam", "race_aip", "race_white", "race_unknown", "hispanic_y", "hispanic_n", "hispanic_u")
 
+total16 <- nrow(new_pred16)
+total17 <- nrow(new_pred17)
+total18 <- nrow(new_pred18)
+
+demo_names <- c("male", "female", "age_18_to_24", "age_25_to_35", "age_36_to_50", "age_51_to_65", "age_66_plus", "race_asian", "race_black", "race_nativeam", "race_aip", "race_white", "race_unknown", "hispanic_y", "hispanic_n", "hispanic_u")
+demo_labels <- c("Male", "% Male", "Female", "% Female", "Age 18-24", "% Age 18-24", "Age 25-35", "% Age 25-35", "Age 36-50", "% Age 36-50", "Age 51-65", "% Age 51-65", "Age 66+", "% Age 66+", "Race Asian", "% Race Asian", "Race Black", "% Race Black", "Race Native American", "% Race Native American", "Race Pacific Islander", "% Race Pacific Islander", "Race White", "% Race White", "Race Unknown", "% Race Unknown", "Hispanic", "% Hispanic", "Not Hispanic", "% Not Hispanic", "Hispanic Unknown", "% Hispanic Unknown")
 
 for (i in demo_names){
   tble16 <- table(new_pred16[[i]])
   tble17 <- table(new_pred17[[i]])
   tble18 <- table(new_pred18[[i]])
   demo <- add_row(demo, "2016" = tble16[2], "2017" = tble17[2], "2018" = tble18[2])
+  demo <- add_row(demo, "2016" = tble16[2]/total16*100, "2017" = tble17[2]/total17*100, "2018" = tble18[2]/total18*100)
 }
 
-row.names(demo) <- demo_names
+row.names(demo) <- demo_labels
 
 print(xtable(demo, caption = "Demographics by Year", type = "latex"), file = paste("/Users/User/Documents/NSQIP Surgical Data/demographics.tex", sep = ""))
--- analytics/outcomes_by_year.R
+++ analytics/outcomes_by_year.R
@@ -4,6 +4,7 @@ x <- c("2016", "2017", "2018")
 colnames(outcomes) <- x
 
 outcome_names <- c("y_discharge", "y_dead", "y_reop", "y_related_reop", "y_readmit", "y_related_readmit", "y_sup_ssi", "y_deep_ssi", "y_organ_ssi", "y_wound_disruption", "y_pneumonia", "y_unplanned_intubation", "y_emb", "y_vent", "y_PRF", "y_ARF", "y_uti", "y_stroke", "y_cpr", "y_mi", "y_trans", "y_thromb", "y_sepsis", "y_sepshock", "y_cdiff")
+outcome_labels <- c("Discharged", "Dead", "Reoperation", "Related Reoperation", "Readmission", "Related Readmission", "Superficial Incisional SS", "Deep Incisional SSI", "Organ SSI", "Wound Disruption", "Pneumonia", "Unplanned Intubation", "Pulmonary Embolism", "Ventilator", "Progressive Renal Failure", "Acute Renal Failure", "Urinary Tract Infection", "Stroke", "Cardiac Arrest Requiring CPR", "Myocardial Infarction", "Bleeding Transfusions", "DVT/Thrombophlebitis", "Sepsis", "Septic Shock", "Clostridium Difficile Colitis")
 
 for (i in outcome_names){
   tble16 <- table(grouped_outcomes_puf16[[i]])
@@ -12,6 +13,6 @@ for (i in outcome_names){
   outcomes <- add_row(outcomes, "2016" = tble16[2], "2017" = tble17[2], "2018" = tble18[2])
 }
 
-row.names(outcomes) <- outcome_names
+row.names(outcomes) <- outcome_labels
 
 print(xtable(outcomes, caption = "Outcomes by Year", type = "latex"), file = paste("/Users/User/Documents/NSQIP Surgical Data/outcomes.tex", sep = ""))
